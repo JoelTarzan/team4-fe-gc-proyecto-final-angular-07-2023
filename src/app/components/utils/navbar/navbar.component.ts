51318606f93def0b7ef9ff210c821e706918e5e6
Joel Tarzán <45524750+JoelTarzan@users.noreply.github.com>
2023-08-03 00:58:20 +0200

    Componente navbar creado y añadido al componente 'home-main'

--- src/app/components/utils/navbar/navbar.component.ts
+++ src/app/components/utils/navbar/navbar.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+
+  rol: string = 'candidate';
+  currentPage: string = '';
+
+  constructor( 
+    private router: Router) {
+
+  }
+
+  ngOnInit(): void {
+    this.router.events.subscribe(event => {
+
+      if (event instanceof NavigationEnd) {
+        this.currentPage = event.url;
+        
+      }
+    });
+  }
 
 }

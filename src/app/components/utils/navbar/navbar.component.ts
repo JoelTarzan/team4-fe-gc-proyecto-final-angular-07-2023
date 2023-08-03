d69f5c44d0e5838fd66b2e268e84d27b48b9e715
Joel Tarzán <45524750+JoelTarzan@users.noreply.github.com>
2023-08-03 16:18:34 +0200

    Borrado codigo que no se usaba

--- src/app/components/utils/navbar/navbar.component.ts
+++ src/app/components/utils/navbar/navbar.component.ts
@@ -1,29 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
 
   rol: string = 'candidate';
-  currentPage: string = '';
-
-  constructor( 
-    private router: Router) {
-
-  }
-
-  ngOnInit(): void {
-    this.router.events.subscribe(event => {
-
-      if (event instanceof NavigationEnd) {
-        this.currentPage = event.url;
-        
-      }
-    });
-  }
 
 }

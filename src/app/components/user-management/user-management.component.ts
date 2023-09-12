f3400ed2205fdbff0ae80f463ac23cb8cab9b8f0
Joel Tarzán <45524750+JoelTarzan@users.noreply.github.com>
2023-09-12 21:29:04 +0200

    Sync de la vista Gestión Usuarios al backend completada

--- src/app/components/user-management/user-management.component.ts
+++ src/app/components/user-management/user-management.component.ts
@@ -12,6 +12,9 @@ export class UserManagementComponent implements OnInit {
   allUsers: User[] = [];
   displayedUsers: User[] = [];
 
+  selectedOption: string = 'name-asc';
+  inputValue: string = '';
+
   numRrhh: number = 0;
   numCandidates: number = 0;
 
@@ -25,22 +28,9 @@ export class UserManagementComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.getUsersNameAsc();
 
-    // Recogemos todos los usuarios
-    this.usersService.getAll().subscribe(result => {
-      this.allUsers = result;
-
-      // Contamos el numero de candidatos y usuarios de RRHH que existen
-      this.allUsers.forEach(user => {
-        user.role.name=="rrhh" ? this.numRrhh++ : this.numCandidates++;
-      });
-
-      // Calculamos las paginas totales
-      this.totalPages = Math.ceil(this.allUsers.length / this.itemsPerPage);
-
-      // Cambiamos los usuarios a mostrar
-      this.onPageChanged(1);
-    });
+    
   }
 
   // Actualiza los usaurios
@@ -58,9 +48,139 @@ export class UserManagementComponent implements OnInit {
   // Guarda los cambios de los usuarios
   saveChanges() {
     this.allUsers.forEach(user => {
+      if (user.role.name == 'candidate') {
+        user.role.id = 3;
+
+      } else if (user.role.name == 'rrhh') {
+        user.role.id = 2;
+      }
+
+      if (user.rating == 0) {
+        user.rating = 1;
+      }
+      
       this.usersService.update(user.id, user).subscribe();
     });
 
-    window.location.reload();
+    setTimeout(() => {
+      window.location.reload();
+    }, 2000);
+  }
+
+  // Controla el filtro de la vista
+  filterChange() {
+    switch (this.selectedOption) {
+      case 'name-asc':
+        this.getUsersNameAsc();
+        break;
+    
+      case 'name-desc':
+        this.getUsersNameDesc();
+        break;
+
+      case 'rrhh':
+        this.getUsersOrderRrhh();
+        break;
+
+      case 'candidate':
+        this.getUsersOrderCandidate();
+        break;
+    }
+  }
+
+  // Recogemos los usuarios por orden ascendente
+  getUsersNameAsc() {
+    this.numRrhh = 0;
+    this.numCandidates = 0;
+
+    // Recogemos todos los usuarios
+    this.usersService.getAllNameAsc().subscribe(result => {
+      this.allUsers = result;
+
+      // Cogemos solo los usuarios que no sean admin
+      this.allUsers = this.allUsers.filter(user => {
+        return user.role.name !== 'admin';
+      });
+
+      // Contamos el numero de candidatos y usuarios de RRHH que existen, lo hacemos aquí ya que es el filtro que se carga por defecto
+      this.allUsers.forEach(user => {
+        user.role.name=="rrhh" ? this.numRrhh++ : this.numCandidates++;
+      });
+
+      this.updateDisplayedUsers();
+    });
+  }
+
+  // Recogemos los usuarios por orden descendente
+  getUsersNameDesc() {
+    // Recogemos todos los usuarios
+    this.usersService.getAllNameDesc().subscribe(result => {
+      this.allUsers = result;
+
+      // Cogemos solo los usuarios que no sean admin
+      this.allUsers = this.allUsers.filter(user => {
+        return user.role.name !== 'admin';
+      });
+
+      this.updateDisplayedUsers();
+    });
+  }
+
+  getUsersOrderRrhh() {
+    // Recogemos todos los usuarios
+    this.usersService.getAllOrderByRole().subscribe(result => {
+      this.allUsers = result;
+
+      // Cogemos solo los usuarios que no sean admin
+      this.allUsers = this.allUsers.filter(user => {
+        return user.role.name !== 'admin';
+      });
+
+      this.updateDisplayedUsers();
+    });
+  }
+
+  getUsersOrderCandidate() {
+    // Recogemos todos los usuarios
+    this.usersService.getAllOrderByRole().subscribe(result => {
+      this.allUsers = result;
+
+      // Cogemos solo los usuarios que no sean admin
+      this.allUsers = this.allUsers.filter(user => {
+        return user.role.name !== 'admin';
+      });
+
+      // Reordenamos el array
+      this.allUsers = this.allUsers.reverse();
+
+      this.updateDisplayedUsers();
+    });
+  }
+
+  onInputChange() {
+    if (this.inputValue == '') {
+      this.filterChange();
+
+    } else {
+      this.usersService.getAllStartingWith(this.inputValue).subscribe(result => {
+        this.allUsers = result;
+
+         // Cogemos solo los usuarios que no sean admin
+        this.allUsers = this.allUsers.filter(user => {
+          return user.role.name !== 'admin';
+        });
+
+        this.updateDisplayedUsers();
+      });
+    }
+  }
+
+  // Actualiza la paginación y los usuarios a mostrar, asi como el contador de tipos de usuarios
+  updateDisplayedUsers() {
+    // Calculamos las paginas totales
+    this.totalPages = Math.ceil(this.allUsers.length / this.itemsPerPage);
+
+    // Cambiamos los usuarios a mostrar
+    this.onPageChanged(1);
   }
 }

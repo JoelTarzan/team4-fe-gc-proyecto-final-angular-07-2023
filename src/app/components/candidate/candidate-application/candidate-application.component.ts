f973e3308b067996de1d3bfc9615d8ec91633dc7
Joel Tarzán <45524750+JoelTarzan@users.noreply.github.com>
2023-09-15 21:44:39 +0200

    Procesos y la barra de procesos ahora funciona apuntando al backend

--- src/app/components/candidate/candidate-application/candidate-application.component.ts
+++ src/app/components/candidate/candidate-application/candidate-application.component.ts
@@ -9,6 +9,7 @@ import { CandidaturesService } from 'src/app/services/candidatures.service';
 import { IdUserOpenProcessService } from 'src/app/services/id-user-open-process.service';
 import { OpenProcessesService } from 'src/app/services/open-processes.service';
 import { SubscriptionsService } from 'src/app/services/subscriptions.service';
+import { TokenStorageService } from 'src/app/services/token-storage.service';
 import { UsersService } from 'src/app/services/users.service';
 
 interface Imagen {
@@ -26,12 +27,16 @@ export class CandidateApplicationComponent implements OnInit {
 
   idApplication: number = 0;
 
+  userLogged: User | undefined;
   application: Application | undefined;
-  user: User | undefined;
+  candidate: User | undefined;
   candidature: Candidature | undefined;
 
   processes: OpenProcess[] = [];
-  processStatus: string = "Entrevista";
+  idCurrentProcess: number = 0;
+
+  newOpenProcessName: string = '';
+  newOpenProcessDate: string = '';
 
   subscriptions:any;
 
@@ -45,7 +50,8 @@ export class CandidateApplicationComponent implements OnInit {
     private idUserProcessService : IdUserOpenProcessService,
     private subscriptionsService: SubscriptionsService,
     private applicationsService: ApplicationsService,
-    private routeActive: ActivatedRoute) {
+    private routeActive: ActivatedRoute, 
+    private tokenStorageService: TokenStorageService) {
 
   }
 
@@ -54,24 +60,75 @@ export class CandidateApplicationComponent implements OnInit {
     this.routeActive.params.subscribe(params => {
       this.idApplication = params['id'] || null;
       
+      // Buscamos la application y recogemos todos los datos necesarios
       this.applicationsService.getById(this.idApplication).subscribe(result => {
 
         this.application = result;
-        this.user = this.application.user;
+        this.candidate = this.application.user;
         this.candidature = this.application.candidature;
 
-        this.openProcessesService.getAllCandidatureAndUserByDateASC(this.candidature.id, this.user.id).subscribe(result => {
+        this.idCurrentProcess = this.application.phase.id!;
+        
+        this.openProcessesService.getAllCandidatureAndUserByDateASC(this.candidature.id, this.candidate.id).subscribe(result => {
           this.processes = result;
         });
       });
     });
 
-    this.subscriptionsService.getAllUser(this.user!.id).subscribe((result: any) => {
-      this.subscriptions = result;
-      console.log(result);
+    // Buscamos las subscripciones de el usuario que esta logueado actualmente
+    this.usersService.getOneById(this.tokenStorageService.getUser()).subscribe(result => {
+
+      this.userLogged = result;
+      
+      this.subscriptionsService.getAllUser(this.userLogged.id).subscribe((result: any) => {
+        this.subscriptions = result;
+      });
     });
   }
 
+  // Crea un proceso nuevo para esta application
+  createOpenProcess() {
+    let newOpenProcess = {
+      candidature: this.candidature!,
+      name: this.newOpenProcessName,
+      date: this.newOpenProcessDate,
+      user: this.candidate!,
+      open: false
+    }
+
+    // Creamos el nuevo proceso
+    this.openProcessesService.create(newOpenProcess).subscribe(() => {
+
+      // Recargamos los procesos
+      this.openProcessesService.getAllCandidatureAndUserByDateASC(this.candidature!.id, this.candidate!.id).subscribe(result => {
+        this.processes = result;
+      });
+
+      this.newOpenProcessName = '';
+      this.newOpenProcessDate = '';
+
+      this.addProcessMode = false;
+    });
+  }
+
+  // Borramos el proceso
+  deleteOpenProcess(process: OpenProcess) {
+    this.openProcessesService.delete(process.id!).subscribe(() => {
+
+      // Recargamos los procesos
+      this.openProcessesService.getAllCandidatureAndUserByDateASC(this.candidature!.id, this.candidate!.id).subscribe(result => {
+        this.processes = result;
+      });
+    });
+  }
+
+  // Cambia el proceso actual
+  changeCurrentProcess(process: OpenProcess) {
+    this.idCurrentProcess = process.id!
+
+    this.application!.phase = process;
+    this.applicationsService.update(this.application!.id, this.application!).subscribe();
+  }
   
   isSubscribed(processId: number): boolean {
     //Verifica que existe
@@ -88,10 +145,10 @@ export class CandidateApplicationComponent implements OnInit {
         //borra en la base de datos desde Service
         this.subscriptionsService.delete(this.subscriptions.find((subscription: { id_open_processes: number; }) => subscription.id_open_processes === processId));
       }else{
-        this.idUserProcessService.addSubscribeUserProcess ({id:99,id_open_processes: processId, id_user:this.user!.id});
+        this.idUserProcessService.addSubscribeUserProcess ({id:99,id_open_processes: processId, id_user:this.userLogged!.id});
       }
     }
-    this.idUserProcessService.getSubscribeFindByUser_Process(this.user!.id).subscribe((result: any) => {
+    this.idUserProcessService.getSubscribeFindByUser_Process(this.userLogged!.id).subscribe((result: any) => {
           this.subscriptions = result;
         });
   }
